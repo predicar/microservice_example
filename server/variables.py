2d963b06563f9c5005384e4d926a6e59e97197a5
Max Karel <maxdkarel@gmail.com>
2019-11-01 12:05:06 +0100

    Redo variables.py to normal and proper way of setting up debug mode for flask:(

--- server/variables.py
+++ server/variables.py
@@ -3,5 +3,5 @@ import os
 app_config = {
             'host': os.getenv('MICROSERVICE_EXAMPLE_HOST', '0.0.0.0'),
             'port': int(os.getenv('MICROSERVICE_EXAMPLE_PORT', 8080)),
-            'debug': True
+            'debug': os.getenv('DEBUG') == "1"
             }
